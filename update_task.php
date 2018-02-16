e5e3dc5e6e0594e4deb5647035e29b9ab4c7398f
lesibe <hansmanaka7@gmail.com>
2018-02-16 17:54:31 +0200

    Update update_task.php

--- update_task.php
+++ update_task.php
@@ -4,4 +4,58 @@
  */
 require('Task.class.php');
 // Assignment: Implement this script
+
+foreach ($_REQUEST as $key => $value) {
+	$$key = trim($value);
+}
+
+$myTask = new Task();
+
+$err = 0;
+$msg = "";
+
+if ($action == "view") {
+
+	
+}
+
+
+if ($action == "save") {
+	
+	$InputTaskName			= preg_replace("/[^a-zA-Z ]/","",$InputTaskName);
+	$InputTaskDescription	= preg_replace("/[^a-zA-Z ]/","",$InputTaskDescription);
+	
+	if ($currentTaskId			== "") {$err++; $msg .= "problem with hidden id.<br>";}
+	if ($InputTaskName     		== "") {$err++; $msg .= "Enter your Task ame.<br>";}
+	if ($InputTaskDescription	== "") {$err++; $msg .= "Enter your Description.<br>";}
+	
+	
+	if ($err == 0) {  // if there are no errors	
+		
+		$result = $myTask->Save($currentTaskId, $InputTaskName, $InputTaskDescription);
+		
+	}else {
+		$result = $msg;
+	}
+
+	echo $result;
+	
+
+}
+
+if ($action == "delete") {
+	
+	$currentTaskId		= preg_replace("/[^0-9 ]/", "", $currentTaskId);
+	
+	if ($currentTaskId	== "") {$err++; $msg .= "problem with hidden id.<br>";}	
+
+	if ($err == 0) {  // if there are no errors	
+		
+		$result = $myTask->Delete($currentTaskId);
+		echo $result;
+	}else{
+		echo $msg;	
+	}
+}
+
 ?>
